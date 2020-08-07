c1ce66f19ce8c987569a19feb5e4c3c810418e27
vikaschowdary47 <vikaschowdary47@gmail.com>
2020-08-07 16:14:58 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
+
 
 // Routes
 const slotRoute = require('./routes/slot')
 
 // middleware
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
 
 
 dotenv.config();
@@ -24,11 +27,19 @@ mongoose.connect(
     () => console.log('connected to mongodb database')
 )
 
-
 // route middleware
 app.use('/api/slot', slotRoute);
 
+
 const port = process.env.PORT || 9000;
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, 'client','build','index.html'));
+    })
+}
 
 app.listen(port, () => {
     console.log(`server running at ${port}`)
